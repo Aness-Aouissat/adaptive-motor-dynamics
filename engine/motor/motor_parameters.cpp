34d04f0b3f5c6e854aed246fe2da84e5ca7889c6
Aness-Aouissat <anessaouissat@gmail.com>
2026-09-24 03:03:09 +0400

    feat: add parameter mutation request handling for drift experiments

--- engine/motor/motor_parameters.cpp
+++ engine/motor/motor_parameters.cpp
@@ -1,4 +1,5 @@
 #include "types.hpp"
+#include "motor_parameters.hpp"
 
 static Electric_Param electric_param{1, 2, 3, 4, 5}; //TBD, filler for now
 static Mechanical_Param mechanical_param{1, 2, 3}; //TBD, filler for now
@@ -16,14 +17,65 @@ const Thermal_Param& get_thermal_params() {
     return thermal_param;
 }
 
-static void mutate_electric_fields() {
-    
+void mutate_electric_fields(const Request& request) {
+
+    for(const auto& [param, delta] : request.electric_map) {
+        switch(param) {
+            case (electric_fields::R_0):
+                electric_param.R_0 += delta;
+                break;
+            case (electric_fields::T_0):
+                electric_param.T_0 += delta;
+                break;
+            case (electric_fields::L_a):
+                electric_param.L_a += delta;
+                break;
+            case (electric_fields::K_e):
+                electric_param.K_e += delta;
+                break;
+            case (electric_fields::alpha):
+                electric_param.alpha += delta;
+                break;
+            default:
+                break;
+        }
+    }
 }   
 
-static void mutate_mechanical_fields() {
+void mutate_mechanical_fields(const Request& request) {
 
+    for(const auto& [param, delta] : request.mechanical_map) {
+        switch(param) {
+            case (mechanical_fields::K_t):
+                mechanical_param.K_t += delta;
+                break;
+            case (mechanical_fields::D):
+                mechanical_param.D += delta;
+                break;
+            case (mechanical_fields::J):
+                mechanical_param.J += delta;
+                break;
+            default:
+                break;
+        }
+    }
 }
 
-static void mutate_thermal_fields() {
+void mutate_thermal_fields(const Request& request) {
 
+    for(const auto& [param, delta] : request.thermal_map) {
+        switch(param) {
+            case (thermal_fields::T_ambient):
+                thermal_param.T_ambient += delta;
+                break;
+            case (thermal_fields::R_th):
+                thermal_param.R_th += delta;
+                break;
+            case (thermal_fields::C_th):
+                thermal_param.C_th += delta;
+                break;
+            default:
+                break;
+        }
+    }
 }
--- engine/motor/motor_parameters.hpp
+++ engine/motor/motor_parameters.hpp
@@ -1,5 +1,36 @@
 #pragma once 
+#include <unordered_map>
 #include "types.hpp"
+
+enum class electric_fields {
+    R_0,
+    T_0,
+    L_a,
+    K_e,
+    alpha,
+};
+
+enum class mechanical_fields {
+    K_t,
+    D,
+    J
+};
+
+enum class thermal_fields {
+    T_ambient,
+    R_th,
+    C_th
+};
+
+typedef struct{
+    std::unordered_map<electric_fields, double> electric_map;
+    std::unordered_map<mechanical_fields, double> mechanical_map;
+    std::unordered_map<thermal_fields, double> thermal_map;
+} Request;
+
+void mutate_electric_fields(const Request&);
+void mutate_mechanical_fields(const Request&);
+void mutate_thermal_fields(const Request&);
 
 const Electric_Param& get_electric_params();
 const Mechanical_Param& get_mechanical_params();
